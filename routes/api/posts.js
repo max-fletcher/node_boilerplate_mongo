b347f30480827daccd0be8c57d1bf4be1f1befdb
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-02-02 18:29:17 +0600

    File upload works but logic is abstract. Need to centralize it.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../../controllers/postController');
+
 // IMPORT MULTER FOR FILE UPLOAD
 const multer = require('multer');
 // STORAGE OPTIONS FOR MULTER
@@ -18,7 +19,7 @@ const storage = multer.diskStorage({
 })
 
 const limits = {
-  fileSize: 28000, // 28 KB MAX
+  fileSize: 10485760, // 10 MB MAX
 }
 
 const allowedMimetypes = [ 'image/png', 'image/jpeg', 'image/jpg', 'image/webp' ]
@@ -60,53 +61,30 @@ const singleFileFilter = (req, file, cb) => {
 }
 
 const singleFileDelayedValidationFilter = (req, file, cb) => {
-
-  let file_errors = [];
-
-  if (!allowedMimetypes.includes(file.mimetype)){
-    console.log('mime');
-    file_errors = [...file_errors, 'Incorrect mimetype.']
-  }
-
-  let match = false
-  for(i = 0; i < allowedExt.length; i++) {
-    if (file.originalname.match(allowedExt[i]))
-      match = true
-  }
-  if(match === false){
-    console.log('ext');
-    file_errors = [...file_errors, 'Incorrect mimetype. Must be of type png, jpeg, jpg or webp']
-  }
-
   const fileSize = parseInt(req.headers["content-length"])
   console.log('file size', fileSize);
-  if(fileSize < 30000){
-    console.log('size');
-    file_errors = [...file_errors, 'File size must be less than 28KB.']
-  }
-
-  console.log('all_errors', file_errors);
-
-  if(file_errors.length !== 0){
-    req.body.file_errors = file_errors
-    return cb(null, false)
-  }
+  if(fileSize > 10485760){
+    // if(fileSize > 100000){
+      console.log('size');
+      req.body.status = 'limit_exceeded'
+      return cb(null, false)
+    }
 
   return cb(null, true)
 }
 // CREATING THE MULTER INSTANCE
 const postImageUpload = multer({ 
                   storage: storage, 
-                  // limits: limits,
-                  // LOGIC FOR IF THE FILE SHOULD BE ALLOWED TO BE UPLOADED OR NOT
-                  fileFilter: singleFileFilter
-              })
+                  limits: limits, 
+                  // LOGIC FOR IF THE FILE SHOULD BE ALLOWED TO BE UPLOADED OR NOT 
+                  fileFilter: singleFileDelayedValidationFilter 
+              }).single('image')
 
 // const fieldsOnly = multer()
 
 router.route('/')
     .get(postController.getAllPosts)
-    .post(postImageUpload.single('image'), postController.createNewPost)
+    .post(postImageUpload, postController.createNewPost)
 
     // fieldsOnly.none()
     // postImageUpload.single('image')
--- validation/schemas/PostSchema.js
+++ validation/schemas/PostSchema.js
@@ -1,13 +1,23 @@
 const { z } = require('zod')
 
 const StorePostSchema = z.object({
-  text: z.string({ required_error: "Post message is required" }).min(10, { message: "Post message is required" }).max(2, { message: "Yolo" }),
+  text: z.string({ required_error: "Post message is required" }).min(10, { message: "Bitch MF" }).max(2, { message: "Yolo" }),
   user_id: z.string(),
   file: z.object({
     fieldname: z.string(),
-    originalname: z.union([z.string().endsWith('.jpg'), z.string().endsWith('.png'), z.string().endsWith('.jpeg'), z.string().endsWith('.webp')]),
-    mimetype: z.union([z.string().endsWith('.jpg'), z.string().endsWith('.jpeg'), z.string().endsWith('.png'), z.string().endsWith('.webp')]),
-    destination: z.union([z.string().includes('image/jpg'), z.string().includes('image/jpeg'), z.string().includes('image/png'), z.string().includes('image/webp')]),
+    originalname: z.union([
+      z.string().endsWith('.jpg', { message: "File type must be jpg, jpeg, png or webp" }),
+      z.string().endsWith('.jpeg', { message: "File type must be jpg, jpeg, png or webp" }),
+      z.string().endsWith('.png', { message: "File type must be jpg, jpeg, png or webp" }),
+      z.string().endsWith('.webp', { message: "File type must be jpg, jpeg, png or webp" })
+    ]),
+    mimetype: z.union([
+      z.string().includes('image/jpg', { message: "File type must be jpg, jpeg, png or webp" }), 
+      z.string().includes('image/jpeg', { message: "File type must be jpg, jpeg, png or webp" }), 
+      z.string().includes('image/png', { message: "File type must be jpg, jpeg, png or webp" }), 
+      z.string().includes('image/webp', { message: "File type must be jpg, jpeg, png or webp" })
+    ]),
+    destination: z.string(),
     filename: z.string(),
     path: z.string(),
     size: z.number().max(30000, { message: "File size must be less than 30KB" }),
